8665939e909255171dca8f978b56bc6908a99a37
bubble0h7 <faydawnmorris@gmail.com>
2019-06-03 15:05:09 +1200

    Renamed home view/routes to admin and protected from guest users

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::get('/', function () {
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware('auth')->group(function () {
+    
+    Route::get('/admin', 'HomeController@index')->name('admin');
+
+});
