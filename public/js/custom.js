97d5fc0315bae7d47791bc3e7107ff3674bd2328
bubble0h7 <faydawnmorris@gmail.com>
2019-06-10 20:28:18 +1200

    Customised welcome page

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,36 @@
+var path = window.location.pathname;
+var page = path.split("/").pop();
+
+if (page == "about") {
+  var word = 'echo Wait... You want to know more?';
+} else {
+  var word = 'echo Hi. Hello.';
+}
+
+var div = document.getElementById('terminal-line');
+
+function readWord(word, el) {
+  var pos = 0;
+  var duration = 250;
+  var text = '$ ';
+  var readLetter = (function readLetter () {
+    if (pos < word.length) {
+        var letter = word.substr(pos, 1);
+        text += letter;
+        div.innerHTML = text;
+        pos++;
+        setTimeout(readLetter, duration);
+    }
+  })();
+}
+
+readWord(word, div);
+
+document.getElementById('menu-right').addEventListener("change", menuItemChange);
+function menuItemChange() {
+  var menu = document.getElementById("menu-right");
+  var selectedItem = menu.options[menu.selectedIndex].value;
+  window.location.href = "http://www.fay.geek.nz/" + selectedItem;
+}
+
 CKEDITOR.replace( 'content' );
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,88 +2,65 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <meta http-equiv="Cache-Control" content="no-cache">
 
-        <title>Fay Morris</title>
+        <title>Portfolio | Fay Morris </title>
+        <meta name="description" content="Homepage of Fay Morris' portfolio website.">
+        <meta name="author" content="Fay Morris">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="img/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="img/favicon-16x16.png" sizes="16x16" />
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+        <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/admin') }}">Admin</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Welcome
-                </div>
-
-                <p>
-                    This site is currently under construction.
-                </p>
+        <nav>
+            <ul>
+                <li><a href="/about">About</a></li>
+                <li><a href="/code">Code</a></li>
+                <li><a href="/index"><span id="active">Terminal</span></a></li>
+                <li id="nav-right">
+                <select id="menu-right">
+                    <option value="" selected>1: bash</option>
+                    <option value="about">2: about</option>
+                    <option value="">3: cv</option>
+                    <option value="">4: contact</option>
+                </select>
+                </li>
+            </ul>
+            </nav>
+        
+            <div class="col-12">
+            <h1><span id="green">bubble0h7@toothless</span> <span id="purple">MINGW64</span> <span id="yellow">/fay/portfolio</span></h1>
+            <h2 id="terminal-line" class="text">$</h2>
             </div>
-        </div>
+        
+            <div class="col-12">
+            <p>
+                This site is under construction.
+            </p>
+            </div>
+            
+            <footer>
+                Fay Morris<br>
+                <span id="green"><a href="mailto:hi@fay.geek.nz">hi@fay.geek.nz</a></span> 
+                @if (Route::has('login'))
+                    <div>
+                        @auth
+                            <a id="blue" href="{{ url('/admin') }}">> admin panel</a>
+                        @else
+                            <a id="blue" href="{{ route('login') }}">> login</a>
+                        @endauth
+                    </div>
+                @endif
+                <img id="coding-fay" src="img/2019-me-glow-glasses-nobackground-cropbottom.png">
+            </footer>
+        
+            <script src="js/custom.js"></script>
     </body>
 </html>
