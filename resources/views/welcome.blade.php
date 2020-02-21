f5b7770a120db4dfcbc4769088dd63804c345a62
bubble0h7 <faydawnmorris@gmail.com>
2020-02-21 18:00:39 +1300

    Revert last commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,17 +8,17 @@
 
     <div class="col-10 offset-1">
             <p>
-                Dec 25 2019 <a href="{{ route('now') }}">now</a><br>
+                drwxr-xr-x 1 fay 197121      0 Dec 25 2019 <a href="{{ route('now') }}">now</a><br>
                 @if (isset($project) && $project != null)
-                @if(date("Y-m-d", strtotime("today")) == date("Y-m-d", strtotime($project->updated_at))){{date("M  j   H:i", strtotime($project->updated_at))}}@else{{date("M  j   Y", strtotime($project->updated_at))}}@endif <a href="{{ route('projects') }}">projects</a><br>
+                drwxr-xr-x 1 fay 197121      0 @if(date("Y-m-d", strtotime("today")) == date("Y-m-d", strtotime($project->updated_at))){{date("M  j   H:i", strtotime($project->updated_at))}}@else{{date("M  j   Y", strtotime($project->updated_at))}}@endif <a href="{{ route('projects') }}">projects</a><br>
                 @endif
                 @if (isset($post) && $post != null)
-                @if(date("Y-m-d", strtotime("today")) == date("Y-m-d", strtotime($post->updated_at))){{date("M  j   H:i", strtotime($post->updated_at))}}@else{{date("M  j   Y", strtotime($post->updated_at))}}@endif <a href="{{ route('blog') }}">blog</a><br>
+                drwxr-xr-x 1 fay 197121      0 @if(date("Y-m-d", strtotime("today")) == date("Y-m-d", strtotime($post->updated_at))){{date("M  j   H:i", strtotime($post->updated_at))}}@else{{date("M  j   Y", strtotime($post->updated_at))}}@endif <a href="{{ route('blog') }}">blog</a><br>
                 @endif
                 @if (isset($file) && $file != null)
-                @if(date("Y-m-d", strtotime("today")) == date("Y-m-d", strtotime($file->updated_at))){{date("M  j   H:i", strtotime($file->updated_at))}}@else{{date("M  j   Y", strtotime($file->updated_at))}}@endif <a href="{{ route('files') }}">files</a><br>
+                drwxr-xr-x 1 fay 197121      0 @if(date("Y-m-d", strtotime("today")) == date("Y-m-d", strtotime($file->updated_at))){{date("M  j   H:i", strtotime($file->updated_at))}}@else{{date("M  j   Y", strtotime($file->updated_at))}}@endif <a href="{{ route('files') }}">files</a><br>
                 @endif
-                Dec 25 2019 <a href="https://github.com/bubble0h7" target="_blank">git</a><br>
+                drwxr-xr-x 1 fay 197121      0 Dec 25 2019 <a href="https://github.com/bubble0h7" target="_blank">git</a><br>
             </p>
     </div>
 @endsection
